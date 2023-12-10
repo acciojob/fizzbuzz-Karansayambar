cef60a6074b191ea9f63104fb01d35afcd3ea1a0
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-10 12:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function FizzBuzz(){
+	for(let i = 1; i <= 20; i++){
+		if(i % 3 == 0){
+			console.log("Fizz");
+		}else if(i % 5 == 0){
+			console.log("Buzz");
+		}else if(i % (3+5) == 0 ){
+			console.log("FizzBuzz");
+		}
+	}
+}
